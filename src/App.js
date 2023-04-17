d79928bf19c7ce395bd886b1c2534638b1888997
Jeril2201 <jerilsamsam01@gmail.com>
2023-04-17 16:34:41 +0530

    cantact page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/Contactus" element={<Contactus/>} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Contactus.js
+++ src/pages/Contactus.js
@@ -5,6 +5,10 @@ import { Container } from "reactstrap";
 import lmg from "../assets/images/Rectangle 40178.svg";
 import Map from "../assets/images/map-pin.png";
 import call from "../assets/images/Vector.png";
+import Logo from'../Asset/images/Group.svg'
+import"../Asset/Sass/Footer.scss";
+import AppStore from '../Asset/images/Frame 6359433.svg'
+import GooglePlay from '../Asset/images/Group 6359429.svg'
 
 const { TabPane } = Tabs;
 function callback(key) {
@@ -15,7 +19,7 @@ const { TextArea } = Input;
 
 const Contactus = () => {
   return (
-    <>
+ <>
       <ConfigProvider
                     theme={{
                       token: {
@@ -28,7 +32,7 @@ const Contactus = () => {
           <h1 className="heading_contact_us">Contact us</h1>
         </center>
         <Row className="pockets_contact_">
-      
+      <div>
           <Col span={12}>
             <p className="heading_here_we_are">
               Here we are for your questions,
@@ -49,12 +53,12 @@ const Contactus = () => {
             </div>
             <img src={lmg} alt="Image" className="pockets_contactus_image" />
 
-            <div className="hi">
-              <div>
+            <div className="pockets_contactus_address">
+              <div className="pockets_contactus_address_1">
                 <h1 className="pockets_united_kingdom">United Kingdom</h1>
 
                 <p className="pockets_address">
-                  {" "}
+              
                   <img src={Map} alt="Map" /> HU11 4UR, England, East Riding of
                   Yorkshire, Great Hatfield
                 </p>
@@ -63,7 +67,7 @@ const Contactus = () => {
                   <img src={call} alt="Map" /> +44 13548910578
                 </p>
               </div>
-              <div>
+              <div className="pockets_contactus_address_2">
                 <h1 className="pockets_united_kingdom">United Kingdom</h1>
 
                 <p className="pockets_address">
@@ -78,8 +82,10 @@ const Contactus = () => {
               </div>
             </div>
           </Col>
-
+          </div>
+             <div>
           <Col span={12} className="pockets_tabspage">
+
             <Card
               className="pockets_card"
               style={{
@@ -95,6 +101,7 @@ const Contactus = () => {
                 style={{ width: "100%" }}
                 className="pockets_tabs_line"
               >
+                
                 <TabPane tab="Contact with us" key="1" className="pockets_tab1">
                   <h4 className="pocktes_email_text">Full Name</h4>
 
@@ -139,11 +146,11 @@ const Contactus = () => {
                   <button className="pocket_contactus_button">Send</button>
                 </TabPane>
 
-                <TabPane tab="Become Our Partner" className="jer" key="2">
+                <TabPane tab="Become Our Partner" key="2">
                   <div
-                    data-aos="flip-left"
-                    data-aos-easing="ease-out-cubic"
-                    data-aos-duration="2000"
+                    // data-aos="flip-left"
+                    // data-aos-easing="ease-out-cubic"
+                    // data-aos-duration="2000"
                   >
                     <div>
                       <h4 className="pocktes_email_text">Full Name</h4>
@@ -194,11 +201,53 @@ const Contactus = () => {
             </Card>
             
           </Col>
-          
+          </div>
         </Row>
-        
+        <div>
+      <footer className='Footer'>
+    <Row justify={'center'}> 
+    <Col md={8}>
+        <div className='Footer_head'>
+        <img src={Logo} alt="image"/>
+<h1>POCKET.</h1>
+        </div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </Col>
+    <Col md={6}>
+        <div className='Footer_li'>
+            <h1>
+            Menu
+            </h1>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>News & Articles</li>
+                <li>Contact us</li>
+            </ul>
+            </div>
+                </Col>
+    <Col md={6}>
+        <div className='Footer_img'>
+            <h1>Join us and let’s make a better world, today</h1>
+           <div><img src={AppStore} alt="image"/></div> 
+           <div><img src={GooglePlay} alt="image"/></div> 
+
+        </div>
+    </Col>
+    
+        </Row>
+        <Row justify={'center'}> 
+            <Col md={20}>
+                <div className='Footer_border'/>
+               <div  className='Footer_End'><span>copyright reserve pockets</span></div> 
+            </Col>
+        </Row>
+    </footer>
+
+      </div>
       </Container>
       </ConfigProvider>
+     
     </>
   );
 };
